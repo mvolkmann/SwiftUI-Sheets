e37b69ab5020df04d5c0243a6ee0cceba1d2c90e
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-11-03 07:46:48 -0500

    Initial Commit

---
+++ SwiftUI-Sheets/SwiftUI_SheetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SheetsApp.swift
+//  SwiftUI-Sheets
+//
+//  Created by R. Mark Volkmann on 11/3/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SheetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
